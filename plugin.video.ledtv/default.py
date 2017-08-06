a324e50b8d2bf7efc080ede85f996c94271e23ac
ledtvavs <ledtv@aerialview.tv>
2017-08-06 13:43:24 -0700

    add on updated

--- plugin.video.ledtv/default.py
+++ plugin.video.ledtv/default.py
@@ -1,63 +1,24 @@
 # -*- coding: utf-8 -*-
-
-""" ^ SECTION 1:
-    This should be at the top of your code to declare the type of text
-    format you're using. Without this you may find some text editors save
-    it in an incompatible format and this can make bug tracking extremely
-    confusing! More info here: https://www.python.org/dev/peps/pep-0263/
-"""
-
 #----------------------------------------------------------------
-
-"""
-    SECTION 2:
-    This is where you'd put your license details, the GPL3 license 
-    is the most common to use as it makes it easy for others to fork
-    and improve upon your code. If you're re-using others code ALWAYS
-    check the license first, removal of licenses is NOT allowed and you
-    generally have to keep to the same license used in the original work
-    (check license details as some do differ).
-
-    Although not all licenses require it (some do, some don't),
-    you should always give credit to the original author(s). Someone may have spent
-    months if not years on the code so really it's the very least you can do if
-    you choose to use their work as a base for your own.
-"""
 # License: GPL (http://www.gnu.org/licenses/gpl-3.0.html)
-# Addon: My YouTube Add-on
-# Author: Add your name here
-
+# Addon: LEDTV
+# Author: Al
 #----------------------------------------------------------------
-
-"""
-    SECTION 3:
-    This is your global imports, any modules you need to import code from
-    are added here. You'll see a handful of the more common imports below.
-"""
 import os           # access operating system commands
+import urlparse     # splits up the directory path - much easier importing this than coding it up ourselves
 import xbmc         # the base xbmc functions, pretty much every add-on is going to need at least one function from here
 import xbmcaddon    # pull addon specific information such as settings, id, fanart etc.
+import xbmcgui      # gui based functions, contains things like creating dialog pop-up windows
 import xbmcplugin   # contains functions required for creating directory structure style add-ons (plugins)
 
-# The following are often used, we are not using them in this particular file so they are commented out
+import koding       # (*) a framework for easy add-on development, this template is to be used in conjunction with this module.
 
-# import re           # allows use of regex commands, if you're intending on scraping you'll need this
-# import xbmcgui      # gui based functions, contains things like creating dialog pop-up windows
+from koding import Add_Dir  # By importing something like this we don't need to use <module>.<function> to call it,
+                            # instead you can just use the function name - in this case Add_Dir().
 
-from koding import route, Addon_Setting, Add_Dir, Find_In_Text, Open_URL, OK_Dialog
-from koding import Open_Settings, Play_Video, Run, Text_File
+addon_id     = xbmcaddon.Addon().getAddonInfo('id')
+dialog       = xbmcgui.Dialog()
 
-#------------------------------------------------------------
-
-"""
-    SECTION 4:
-    These are our global variables, anything we set here can be accessed by any of
-    our functions later on. Please bare in mind though that if you change the value
-    of a global variable from inside a function the value will revert back to the
-    value set here once that function has completed.
-"""
-debug        = Addon_Setting(setting='debug')       # Grab the setting of our debug mode in add-on settings
-addon_id     = xbmcaddon.Addon().getAddonInfo('id') # Grab our add-on id
 
 # Set the base plugin url you want to hook into
 BASE  = "plugin://plugin.video.youtube/playlist/"
@@ -70,38 +31,23 @@ YOUTUBE_CHANNEL_ID_3 = "PLfb8E7lARImvzrAq0O6jyQcstw7aKZvLq"
 YOUTUBE_CHANNEL_ID_4 = "PLlJDPmb6OexrDnAFNBNJez8zRe4EECDZY"
 YOUTUBE_CHANNEL_ID_5 = "UCBDT-Kl4nHgEvpwYfLb315w"
 
-#----------------------------------------------------------------
+master_modes = {
+# Required for certain koding functions to work
+    "play_video"    : "koding.Play_Video(url)",
+    "show_tutorial" : "koding.Show_Tutorial(url)",
+    "tutorials"     : "koding.Grab_Tutorials()",
+# Our custom functions created in this file
+    "simple_dialog" : "Simple_Dialog(url)",
+}
 
-"""
-    SECTION 5:
-    Add our custom functions in here, it's VERY important these go in this section
-    as the code in section 6 relies on these functions. If that code tries to run
-    before these functions are declared the add-on will fail.
 
-    You'll notice each function in here has a decorator above it (an @route() line of code),
-    this assigns a mode to the function so it can be called with Add_Dir and it also tells
-    the code what paramaters to send through. For example you'll notice the Main_Menu() function
-    we've assigned to the mode "main" - this means if we ever want to get Add_Dir to open that
-    function we just use the mode "main". This particular function does not require any extra
-    params to be sent through but if you look at the Simple_Dialog() function you'll see we send through
-    2 different paramaters (title and msg), if you look at the Add_Dir function in Main_Menu()
-    on line 106 you'll see we've sent these through as a dictionary. Using that same format you can send
-    through as many different params as you wish.
-"""
-
-#----------------------------------------------------------------
-# This is the main menu we open into
-@route(mode='main_menu')
 def Main_Menu():
-
-# If debug mode is enabled show the koding tutorials
-    if debug == 'true':
-        Add_Dir ( '[COLOR=lime]Koding Tutorials[/COLOR]', '', "tutorials", True, '', '', '' )
-    else:
-        Add_Dir ( '[COLOR=lime]Enable debug mode for some cool dev tools![/COLOR]', '', "koding_settings", False, '', '', '' )
+# Uncomment the following line for help creating your add-on
+    # Add_Dir(name='KODING TUTORIALS', url='', mode='tutorials', folder=True, icon=os.path.join(art_path,'icon.png'), fanart=os.path.join(art_path,'fanart.jpg'))
     
 # An example title/message we're going to send through to a popup dialog in the first Add_Dir item
-    my_message= "{'title' : 'Support & Suggestions', 'msg' : \"If you come across any online content you'd like to get added please use the support thread at noobsandnerds.com and I'll be happy to look into it for you.\"}"
+    my_message= "{'title' : 'LIVE EVENTS', 'msg' : 'This section is a work in progress, please keep an eye on the forum at noobsandnerds.com for all the latest updates'}"
+
 
     Add_Dir(
         name="Support/Suggestions", url=my_message, mode="simple_dialog", folder=False,
@@ -129,24 +75,41 @@ def Main_Menu():
         name="Uncut Angling Channel", url=BASE2+YOUTUBE_CHANNEL_ID_5+"/", folder=True,
         icon="https://yt3.ggpht.com/-8Rt7LjDZrtU/AAAAAAAAAAI/AAAAAAAAAAA/-0-9PLOJWXM/s100-c-k-no-mo-rj-c0xffffff/photo.jpg")
 #----------------------------------------------------------------
-# A basic OK Dialog
-@route(mode='koding_settings')
-def Koding_Settings():
-    Open_Settings()
-#----------------------------------------------------------------
-# A basic OK Dialog
-@route(mode='simple_dialog', args=['title','msg'])
-def Simple_Dialog(title,msg):
-    OK_Dialog(title, msg)
-#----------------------------------------------------------------
+def Simple_Dialog(my_vars):
+    my_vars = eval(my_vars)
+    title   = my_vars['title']
+    message = my_vars['msg']
+    dialog.ok(title, message)
 
-"""
-    SECTION 6:
-    Essential if creating list items, this tells kodi we're done creating our list items.
-    The list will not populate without this. In the run command you need to set default to
-    whatever route you want to open into, in this example the 'main_menu' route which opens the
-    Main_Menu() function up at the top.
-"""
-if __name__ == "__main__":
-    Run(default='main_menu')
-    xbmcplugin.endOfDirectory(int(sys.argv[1]))
+#----------------------------------------------------------------
+params = dict(urlparse.parse_qsl(sys.argv[2].replace('?', '')))
+
+try: name           = params["name"]
+except: name        = ''
+
+try: description    = params["description"]
+except: description = name
+
+try: fanart         = params["fanart"]
+except: fanart      = ''
+
+try: iconimage      = params["iconimage"]
+except: iconimage   = ''
+
+try: mode           = params["mode"]
+except: mode        = None
+
+try: url            = params["url"]
+except: url         = ''
+
+if mode in master_modes:
+    try:
+        eval(master_modes[mode])
+    except:
+        koding.Text_Box('ERROR IN CODE',koding.Last_Error())
+elif mode == None:
+    Main_Menu()
+else:
+    dialog.ok('MODE DOES NOT EXIST','The following mode does not exist in your master_modes dictionary:','[COLOR=dodgerblue]%s[/COLOR]'%mode)
+
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
